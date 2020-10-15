a349e90b0103bf25af96473c3f242fd64a50798b
StripeCat2503 <khanhduyx371@gmail.com>
2020-10-15 21:33:33 +0700

    Set font family of app

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -7,9 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: KidApp()
+      home: KidApp(),
+      theme: ThemeData(fontFamily: 'Open_Sans'),
     );
   }
 }
-
-
