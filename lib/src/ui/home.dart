9356387f8ba90710cee33934b9478a56cfc6862d
StripeCat2503 <khanhduyx371@gmail.com>
2020-10-15 16:07:10 +0700

    Fixed _appList() function

--- lib/src/ui/home.dart
+++ lib/src/ui/home.dart
@@ -1,57 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:kid_management/src/resources/constant.dart' as CONSTANT;
-final String APP_CONTROL = "App control";
-final String WEB_FILTER = "Web Filter";
-final String SCREEN_BLOCKING = "Screen Blocking";
-final String LOCATION_TRACKING = "Location Tracking";
-final String SCHEDULE = "Schedule";
-final String ACTIVITY_REPORT = "Activity Report";
-
-Widget _title() {
-  return Column(
-    children: <Widget>[
-      Image.asset(
-        CONSTANT.URL_IMG_KID_SPACE_LOGO,
-        width: 150,
-        height: 150,
-      ),
-    ],
-  );
-}
-
-Widget _appList(String title, String img) {
-  return Container(
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(5.0),
-      border: Border.all(color: Colors.grey.withOpacity(0.5), width: 3.0),
-    ),
-    child: RaisedButton(
-        onPressed: () {
-          //TODO ADD EVENT
-        },
-        color: Colors.white.withOpacity(0.95),
-        child: Column(
-          children: [
-            Image.asset(
-              img,
-              width: 100,
-              height: 100,
-            ),
-            SizedBox(height: 10),
-            Text(
-              title,
-              style: TextStyle(
-                fontFamily: 'Segoe UI',
-                fontSize: 12,
-              ),
-            )
-          ],
-        )),
-  );
-}
+import 'package:kid_management/src/ui/app-schedule/app-schedule.dart';
 
 class HomePage extends StatelessWidget {
+  final String APP_CONTROL = "App control";
+  final String WEB_FILTER = "Web Filter";
+  final String SCREEN_BLOCKING = "Screen Blocking";
+  final String LOCATION_TRACKING = "Location Tracking";
+  final String SCHEDULE = "Schedule";
+  final String ACTIVITY_REPORT = "Activity Report";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -70,12 +28,15 @@ class HomePage extends StatelessWidget {
                         mainAxisSpacing: 10.0,
                         crossAxisCount: 2,
                         children: [
-                          _appList(APP_CONTROL, CONSTANT.URL_IMG_APP_CONTROL),
-                          _appList(WEB_FILTER, CONSTANT.URL_IMG_WEB_FILTER),
-                          _appList(SCREEN_BLOCKING, CONSTANT.URL_IMG_SCREEN_BLOCKING),
-                          _appList(LOCATION_TRACKING, CONSTANT.URL_IMG_LOCATION_TRACKING),
-                          _appList(SCHEDULE, CONSTANT.URL_IMG_SCHEDULE),
-                          _appList(ACTIVITY_REPORT, CONSTANT.URL_IMG_ACTIVITY_REPORT)
+                          _appList(APP_CONTROL, CONSTANT.URL_IMG_APP_CONTROL, AppSchedule(), context),
+                          _appList(WEB_FILTER, CONSTANT.URL_IMG_WEB_FILTER, null, context),
+                          _appList(SCREEN_BLOCKING,
+                              CONSTANT.URL_IMG_SCREEN_BLOCKING, null, context),
+                          _appList(LOCATION_TRACKING,
+                              CONSTANT.URL_IMG_LOCATION_TRACKING, null, context),
+                          _appList(SCHEDULE, CONSTANT.URL_IMG_SCHEDULE, null, context),
+                          _appList(
+                              ACTIVITY_REPORT, CONSTANT.URL_IMG_ACTIVITY_REPORT, null, context)
                         ]),
                   ),
                 ],
@@ -84,6 +45,51 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _title() {
+    return Column(
+      children: <Widget>[
+        Image.asset(
+          CONSTANT.URL_IMG_KID_SPACE_LOGO,
+          width: 150,
+          height: 150,
+        ),
+      ],
+    );
+  }
+
+  Widget _appList(String title, String img, Widget screen, BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.0),
+        border: Border.all(color: Colors.grey.withOpacity(0.5), width: 3.0),
+      ),
+      child: RaisedButton(
+          onPressed: () {
+            // route to given screen
+            Navigator.push(context, MaterialPageRoute(builder: (context) => screen,));
+          },
+          color: Colors.white.withOpacity(0.95),
+          child: Column(
+            children: [
+              Image.asset(
+                img,
+                width: 100,
+                height: 100,
+              ),
+              SizedBox(height: 10),
+              Text(
+                title,
+                style: TextStyle(
+                  fontFamily: 'Segoe UI',
+                  fontSize: 12,
+                ),
+              )
+            ],
+          )),
     );
   }
 }
