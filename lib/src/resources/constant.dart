10a572201473857788db8209cecdcb1a68d8f630
StripeCat2503 <khanhduyx371@gmail.com>
2020-10-15 16:06:45 +0700

    Add image url constants

--- lib/src/resources/constant.dart
+++ lib/src/resources/constant.dart
@@ -1,17 +1,31 @@
 /* URL IMAGES WELCOME SCREEN*/
-const String URL_IMG_BACK_GROUND = "assets/images/welcome_screen/backGround.png";
-const String URL_IMG_KID_SPACE_LOGO = "assets/images/welcome_screen/kidSpace_logo.png";
+const String URL_IMG_BACK_GROUND =
+    "assets/images/welcome_screen/backGround.png";
+const String URL_IMG_KID_SPACE_LOGO =
+    "assets/images/welcome_screen/kidSpace_logo.png";
 
 /* URL IMAGES LOGIN SCREEN*/
 const String URL_IMG_EMAIL_ICON = "assets/images/login_screen/user_avatar.png";
-const String URL_IMG_PASSWORD_ICON = "assets/images/login_screen/password_icon.png";
+const String URL_IMG_PASSWORD_ICON =
+    "assets/images/login_screen/password_icon.png";
 const String URL_IMG_USER_AVATAR = "assets/images/login_screen/user_avatar.png";
 
 /* URL IMAGES MAIN DASHBOARD*/
-const String URL_IMG_ACTIVITY_REPORT = "assets/images/main_dashboard/activity_report.png";
-const String URL_IMG_APP_CONTROL = "assets/images/main_dashboard/app_control.png";
-const String URL_IMG_LOCATION_TRACKING = "assets/images/main_dashboard/location_tracking.png";
+const String URL_IMG_ACTIVITY_REPORT =
+    "assets/images/main_dashboard/activity_report.png";
+const String URL_IMG_APP_CONTROL =
+    "assets/images/main_dashboard/app_control.png";
+const String URL_IMG_LOCATION_TRACKING =
+    "assets/images/main_dashboard/location_tracking.png";
 const String URL_IMG_SCHEDULE = "assets/images/main_dashboard/schedule.png";
-const String URL_IMG_SCREEN_BLOCKING = "assets/images/main_dashboard/screen_blocking.png";
+const String URL_IMG_SCREEN_BLOCKING =
+    "assets/images/main_dashboard/screen_blocking.png";
 const String URL_IMG_WEB_FILTER = "assets/images/main_dashboard/web_filter.png";
 
+// IMAGE URL APP SCHEDULE SCREEN
+const String URL_IMG_BG_APP_SCHEDULE_EMPTY =
+    "assets/images/app-schedule/schedule-empty.svg";
+const String URL_IMG_BG_APP_SCHEDULE_STEP01 =
+    "assets/images/app-schedule/bg-create-schedule-1.svg";
+const String URL_IMG_BG_APP_SCHEDULE_STEP03 =
+    "assets/images/app-schedule/bg-create-schedule-3.svg";
