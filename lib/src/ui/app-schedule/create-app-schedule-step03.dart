0ba75f61c9a819d7ce2a4d4d40567bef118f3eff
StripeCat2503 <khanhduyx371@gmail.com>
2020-10-18 14:02:25 +0700

    Fixed time period time picker

--- lib/src/ui/app-schedule/create-app-schedule-step03.dart
+++ lib/src/ui/app-schedule/create-app-schedule-step03.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
 import 'package:kid_management/src/resources/colors.dart';
 import 'package:kid_management/src/ui/app-schedule/create-app-schedule-step02.dart';
 import 'package:kid_management/src/ui/app-schedule/time-picker-button.dart';
@@ -91,9 +94,7 @@ class _CreateAppScheduleStep03State extends State<CreateAppScheduleStep03> {
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(25.0)),
                   elevation: 2.0,
-                  onPressed: () {
-                    
-                  },
+                  onPressed: () {},
                   color: AppColor.mainColor,
                   child: Text(
                     'DONE',
@@ -113,26 +114,111 @@ class TimePeriodPicker extends StatefulWidget {
   DateTime fromTime;
   DateTime toTime;
 
-  DateTime get getFromTime => fromTime;
-  DateTime get getToTime => toTime;
+  String fromButtonText = 'FROM';
+  String toButtonText = 'TO';
 
   @override
   _TimePeriodPickerState createState() => _TimePeriodPickerState();
 }
 
 class _TimePeriodPickerState extends State<TimePeriodPicker> {
+  Widget _startTimePickerButton() {
+    return Container(
+      width: 150.0,
+      child: RaisedButton(
+        elevation: 3,
+        color: widget.fromTime == null ? Colors.white : AppColor.mainColor,
+        onPressed: () {
+          DatePicker.showTime12hPicker(
+            context,
+            currentTime:
+                widget.fromTime == null ? DateTime.now() : widget.fromTime,
+            locale: LocaleType.en,
+            onConfirm: (time) {
+              setState(() {
+                widget.fromTime = time;
+                String time12Hours =
+                    DateFormat('hh:mm a').format(widget.fromTime);
+                widget.fromButtonText = time12Hours;
+              });
+            },
+          );
+        },
+        child: Text(
+          widget.fromButtonText,
+          style: TextStyle(
+              color:
+                  widget.fromTime == null ? AppColor.grayDark : Colors.white),
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+            side: BorderSide(
+                style: BorderStyle.solid, color: AppColor.mainColor, width: 2)),
+      ),
+    );
+  }
+
+  Widget _endTimePickerButton() {
+    return Container(
+      width: 150.0,
+      child: RaisedButton(
+        elevation: 3,
+        color: widget.toTime == null ? Colors.white : AppColor.mainColor,
+        onPressed: () {
+          if (widget.fromTime != null) {
+            DatePicker.showTime12hPicker(
+              context,
+              currentTime:
+                  widget.toTime == null ? DateTime.now() : widget.toTime,
+              locale: LocaleType.en,
+              onConfirm: (time) {
+                Duration duration = widget.fromTime.difference(time);
+                int diffTimeInSeconds = duration.inSeconds * -1;
+                if (diffTimeInSeconds > 0) {
+                  setState(() {
+                    widget.toTime = time;
+                    String time12Hours =
+                        DateFormat('hh:mm a').format(widget.toTime);
+                    widget.toButtonText = time12Hours;
+                  });
+                } else {
+                  Fluttertoast.showToast(
+                      msg: 'Woops! End time must be greater than Start time',
+                      gravity: ToastGravity.CENTER,
+                      backgroundColor: AppColor.grayDark,
+                      textColor: Colors.white);
+                }
+              },
+            );
+          } else {
+            Fluttertoast.showToast(
+                msg: 'Oh no, you forgot choosing start time first',
+                gravity: ToastGravity.CENTER,
+                backgroundColor: AppColor.grayDark,
+                textColor: Colors.white);
+          }
+        },
+        child: Text(
+          widget.toButtonText,
+          style: TextStyle(
+              color: widget.toTime == null ? AppColor.grayDark : Colors.white),
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+            side: BorderSide(
+                style: BorderStyle.solid, color: AppColor.mainColor, width: 2)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        TimePickerButton(
-          buttonText: 'FROM',
-        ),
+        _startTimePickerButton(),
         Icon(Icons.keyboard_arrow_right),
-        TimePickerButton(
-          buttonText: 'TO',
-        )
+        _endTimePickerButton()
       ],
     );
   }
