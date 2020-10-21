bff5f4a3a0f919e3de1058ba632cd28aab098194
StripeCat2503 <khanhduyx371@gmail.com>
2020-10-21 08:30:52 +0700

    Moved main.dart dir

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import 'ui/kid_app.dart';
+import 'src/ui/kid_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
