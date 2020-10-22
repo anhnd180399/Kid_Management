8d1592fd7438c76c8e5bb248a671cacbbb786c88
StripeCat2503 <khanhduyx371@gmail.com>
2020-10-22 13:58:57 +0700

    [LocationTracking] Added filter popup menu

--- lib/src/ui/location-tracking/location_tracking_screen.dart
+++ lib/src/ui/location-tracking/location_tracking_screen.dart
@@ -17,7 +17,6 @@ class LocationTrackingScreen extends StatefulWidget {
 }
 
 class _LocationTrackingScreenState extends State<LocationTrackingScreen> {
-
   bool _searchBarIsShow = false;
 
   @override
@@ -45,30 +44,37 @@ class _LocationTrackingScreenState extends State<LocationTrackingScreen> {
           padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
           child: Column(
             children: [
-              SvgPicture.asset('assets/images/location-tracking/bg_location.svg'),
-              SizedBox(height: 20.0,),
+              SvgPicture.asset(
+                  'assets/images/location-tracking/bg_location.svg'),
+              SizedBox(
+                height: 20.0,
+              ),
               Row(
                 children: [
-                  !_searchBarIsShow ? Text(
-                    'LOCATION HISTORY',
-                    style:
-                    TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
-                  ) : Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: AppColor.grayLight,
-                        borderRadius: BorderRadius.circular(30.0)
-                      ),
-                      child: TextField(
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          hintText: 'Search location history',
-                          border: InputBorder.none,
+                  !_searchBarIsShow
+                      ? Text(
+                          'LOCATION HISTORY',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16.0),
+                        )
+                      : Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: AppColor.grayLight,
+                                borderRadius: BorderRadius.circular(30.0)),
+                            child: TextField(
+                              textAlign: TextAlign.center,
+                              decoration: InputDecoration(
+                                hintText: 'Search location history',
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
+                  Visibility(
+                    child: Spacer(),
+                    visible: !_searchBarIsShow,
                   ),
-                  Visibility(child: Spacer(), visible: !_searchBarIsShow,),
                   IconButton(
                       icon: Icon(
                         Icons.search,
@@ -79,12 +85,8 @@ class _LocationTrackingScreenState extends State<LocationTrackingScreen> {
                           _searchBarIsShow = !_searchBarIsShow;
                         });
                       }),
-                  IconButton(
-                      icon: Icon(
-                        Icons.filter_alt_rounded,
-                        color: AppColor.mainColor,
-                      ),
-                      onPressed: () {})
+                  // filter button
+                  FilterPopupButton()
                 ],
               ),
               LocationHistoryList(
@@ -106,13 +108,61 @@ class _LocationTrackingScreenState extends State<LocationTrackingScreen> {
                       borderRadius: BorderRadius.circular(10.0),
                       side: BorderSide(color: Colors.redAccent)),
                   color: Colors.white,
-                  child: Text('CLEAR HISTORY', style: TextStyle(color: Colors.redAccent),),
+                  child: Text(
+                    'CLEAR HISTORY',
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
                 ),
               )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class FilterPopupButton extends StatefulWidget {
+
+
+  @override
+  _FilterPopupButtonState createState() => _FilterPopupButtonState();
+}
+
+class _FilterPopupButtonState extends State<FilterPopupButton> {
+
+  bool _filterDaysActive = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      onCanceled: () {
+        print('cancel');
+      },
+      child: Icon(
+        Icons.filter_alt_rounded,
+        color: AppColor.mainColor,
+      ),
+      onSelected: (value) {
+
+      },
+      initialValue: 1,
+      itemBuilder: (context) {
+        return [
+          PopupMenuItem(
+              child: RadioListTile(
+            title: Text('Days'),
+            activeColor: AppColor.mainColor,
+            value: 1,
+          )),
+          PopupMenuItem(
+              child: RadioListTile(
+                title: Text('Weeks'),
+                value: 2,
+              )),
+
+        ];
+      },
     );
   }
 }
