7fa588d14def480d5aa1d8b4114c21f1d69352fa
Duy Anh <duyanhnguyen1831998@gmail.com>
2020-10-30 08:26:30 +0700

    [AnhND] add user profile screen

--- lib/src/ui/user-profile/user_profile.dart
+++ lib/src/ui/user-profile/user_profile.dart
@@ -16,15 +16,17 @@ class UserProfileScreen extends StatefulWidget {
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
-
   FirebaseAuth auth = FirebaseAuth.instance;
   TextEditingController _textEditingController;
   bool _editName = false;
+  bool _blacklistNotifyOn = false;
+  bool _notificationNoneDropDown = false;
+  bool _protectedNoneDropDown = false;
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    _textEditingController = new TextEditingController(
-        text: widget.user.fullName);
+    _textEditingController =
+        new TextEditingController(text: widget.user.fullName);
   }
 
   Widget _signOutButton() {
@@ -63,82 +65,266 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
 
   Widget _settings() {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 20.0),
-      padding: EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-          color: AppColor.grayLight, borderRadius: BorderRadius.circular(10.0)),
-      height: 280.0,
-      width: double.infinity,
       child: Column(
         children: [
           Row(
             children: [
-              Text(
-                'SETTINGS',
-                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              Icon(
+                Icons.vibration,
+                color: AppColor.mainColor,
+                size: 30,
+              ),
+              Text('VIBRATION',
+                  style:
+                  TextStyle(fontWeight: FontWeight.w600, fontSize: 16.0)),
+              Padding(padding: const EdgeInsets.symmetric(horizontal: 80.0)),
+              IconButton(
+                icon: Icon(
+                  !_notificationNoneDropDown ? Icons.keyboard_arrow_down : Icons.keyboard_arrow_right,
+                  size: 40,
+                  color: AppColor.mainColor,
+                ),
+                onPressed: (){
+                  setState(() {
+                    _notificationNoneDropDown = !_notificationNoneDropDown;
+                  });
+                },
               )
             ],
-          )
+          ),
+          Visibility(
+            visible: !_notificationNoneDropDown,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0)),
+                    Text('VIBRATE WHEN KID TRY USING blocked APPS',
+                        style:
+                        TextStyle(fontWeight: FontWeight.w600, fontSize: 10.0)),
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 20.0)),
+                    Checkbox(
+                      value: _blacklistNotifyOn,
+                      onChanged: (value) {
+                        setState(() {
+                          _blacklistNotifyOn = value;
+                        });
+                      },
+                      activeColor: AppColor.mainColor,
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0)),
+                    Text('VIBRATE WHEN KID TRY browsing BLACKLIST sites',
+                        style:
+                        TextStyle(fontWeight: FontWeight.w600, fontSize: 10.0)),
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 8.0)),
+                    Checkbox(
+                      value: _blacklistNotifyOn,
+                      onChanged: (value) {
+                        setState(() {
+                          _blacklistNotifyOn = value;
+                        });
+                      },
+                      activeColor: AppColor.mainColor,
+                    )
+                  ],
+                ),
+              ],
+            )
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.wifi_protected_setup,
+                color: AppColor.mainColor,
+                size: 30,
+              ),
+              Text('ACCESS PROTECTION',
+                  style:
+                  TextStyle(fontWeight: FontWeight.w600, fontSize: 16.0)),
+              Padding(padding: const EdgeInsets.symmetric(horizontal: 43.0)),
+              IconButton(
+                icon: Icon(
+                  !_protectedNoneDropDown ? Icons.keyboard_arrow_down : Icons.keyboard_arrow_right,
+                  size: 40,
+                  color: AppColor.mainColor,
+                ),
+                onPressed: (){
+                  setState(() {
+                    _protectedNoneDropDown = !_protectedNoneDropDown;
+                  });
+                },
+              )
+            ],
+          ),
+          Visibility(
+            visible: !_protectedNoneDropDown,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0)),
+                    Text('PREVENT FROM UNISTALLING KIDSPACE',
+                        style:
+                        TextStyle(fontWeight: FontWeight.w600, fontSize: 10.0)),
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 30.0)),
+                    Checkbox(
+                      value: _blacklistNotifyOn,
+                      onChanged: (value) {
+                        setState(() {
+                          _blacklistNotifyOn = value;
+                        });
+                      },
+                      activeColor: AppColor.mainColor,
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0)),
+                    Text('ALLOW SHUTTING DOWN DEVICE',
+                        style:
+                        TextStyle(fontWeight: FontWeight.w600, fontSize: 10.0)),
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 46.0)),
+                    Checkbox(
+                      value: _blacklistNotifyOn,
+                      onChanged: (value) {
+                        setState(() {
+                          _blacklistNotifyOn = value;
+                        });
+                      },
+                      activeColor: AppColor.mainColor,
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0)),
+                    Text('BLOCK SETTINGS APP',
+                        style:
+                        TextStyle(fontWeight: FontWeight.w600, fontSize: 10.0)),
+                    Padding(padding: const EdgeInsets.symmetric(horizontal: 74.0)),
+                    Checkbox(
+                      value: _blacklistNotifyOn,
+                      onChanged: (value) {
+                        setState(() {
+                          _blacklistNotifyOn = value;
+                        });
+                      },
+                      activeColor: AppColor.mainColor,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 
+  Widget _notificationAllert() {
+    return Row(
+      children: [
+        Icon(
+          Icons.notifications,
+          color: AppColor.mainColor,
+          size: 30,
+        ),
+        Text('NOTIFICATION',
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16.0)),
+        Padding(padding: const EdgeInsets.symmetric(horizontal: 60.0)),
+        Switch(
+          value: _blacklistNotifyOn,
+          onChanged: (value) {
+            setState(() {
+              _blacklistNotifyOn = value;
+            });
+          },
+          activeColor: AppColor.mainColor,
+        )
+      ],
+    );
+  }
+
+  Widget _changePassword() {
+    return InkWell(
+        onTap: () {},
+        child: Row(children: [
+          Icon(
+            Icons.accessible,
+            color: AppColor.mainColor,
+            size: 30,
+          ),
+          Text('CHANGE PASSWORD',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20.0)),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          ),
+          Icon(
+            Icons.accessible,
+            color: AppColor.mainColor,
+            size: 30,
+          )
+        ]));
+  }
+
   Widget _fullNameTextField() {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      margin: EdgeInsets.only(bottom: 30.0),
-      child: Stack(
-        children: [
-          TextField(
-            controller: _textEditingController,
-            readOnly: !_editName,
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-            decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                        style: BorderStyle.solid,
-                        width: 2.0,
-                        color: AppColor.mainColor)),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                        style: BorderStyle.solid,
-                        width: 2.0,
-                        color: AppColor.mainColor))),
-          ),
-          Positioned(
-            top: 25,
-            right: 0,
-            child:
-            !_editName ? IconButton(
-                icon: Icon(
-                  Icons.edit,
-                  size: 30,
-                  color: AppColor.mainColor,
-                ),
-                onPressed: () {
-                  setState(() {
-                    _editName = true;
-                  });
-                })
-            : IconButton(
-                icon: Icon(
-                  Icons.check,
-                  size: 30,
-                  color: AppColor.mainColor,
-                ),
-                onPressed: () {
-                  setState(() {
-                    _editName = false;
-                  });
-                })
-          ),
-
-        ],
-      )
-
-    );
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        margin: EdgeInsets.only(bottom: 30.0),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                TextField(
+                    controller: _textEditingController,
+                    readOnly: !_editName,
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                    decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              style: BorderStyle.solid,
+                              width: 1.0,
+                              color: AppColor.mainColor)),
+                      border: InputBorder.none,
+                    )),
+                Positioned(
+                    top: 10,
+                    right: 5,
+                    child: !_editName
+                        ? IconButton(
+                            icon: Icon(
+                              Icons.edit,
+                              size: 25,
+                              color: AppColor.mainColor,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _editName = true;
+                              });
+                            })
+                        : IconButton(
+                            icon: Icon(
+                              Icons.check,
+                              size: 30,
+                              color: AppColor.mainColor,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _editName = false;
+                              });
+                            }))
+              ],
+            ),
+            Text(widget.user.email)
+          ],
+        ));
   }
 
   @override
@@ -150,21 +336,20 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
         child: SingleChildScrollView(
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 20.0),
-              child: SvgPicture.asset(
-                'assets/images/welcome_screen/kidspace_logo_filled.svg',
-                width: 80.0,
-              ),
-            ),
-            // Textfield to display user's full name
-            // and also let user update full name through this field whenever user tap submit button
-            _fullNameTextField(),
-            // body of user profile
-            _settings(),
-
-            _signOutButton()
-          ]),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 20.0),
+                  child: SvgPicture.asset(
+                    'assets/images/welcome_screen/kidspace_logo_filled.svg',
+                    width: 80.0,
+                  ),
+                ),
+                    _fullNameTextField(),
+                    _changePassword(),
+                    SizedBox(height: 10),
+                    _notificationAllert(),
+                    _settings(),
+                    _signOutButton()
+              ]),
         ),
       ),
     );
