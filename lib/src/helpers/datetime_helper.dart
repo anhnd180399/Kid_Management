16d14087d0a5e7e8f8d8cd7d5900c13683d85601
StripeCat2503 <khanhduyx371@gmail.com>
2020-11-04 17:47:08 +0700

    Added DateTimeHelper

---
+++ lib/src/helpers/datetime_helper.dart
@@ -0,0 +1,33 @@
+class DateTimeHelper {
+  // convert 12-hours time format string to Datetime obj
+  static DateTime toDateTime(String timeString) {
+    String startTimeString = timeString.split(' ')[0];
+    // AM or PM
+    String startTimeMode = timeString.split(' ')[1];
+
+    String startHourString = startTimeString.split(':')[0];
+    startHourString =
+        startHourString.length == 1 ? '0$startHourString' : startHourString;
+
+    startHourString = startTimeMode == 'AM'
+        ? startHourString
+        : (int.parse(startHourString) + 12).toString();
+    String startMinuteString = startTimeString.split(':')[1];
+
+    String currentDateString = DateTime.now().toString().split(' ')[0];
+
+    DateTime dateTimeObj = DateTime.parse(
+        '$currentDateString $startHourString:$startMinuteString:00');
+
+    return dateTimeObj;
+  }
+
+  static String minuteToString(int minutes) {
+    var d = Duration(minutes: minutes);
+    List<String> parts = d.toString().split(':');
+    parts.forEach((element) {
+      print(element);
+    });
+    return '${parts[0].padLeft(2, '0')}:${parts[1].padLeft(2, '0')}';
+  }
+}
